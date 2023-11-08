ac59b2a7ff811569850ea72e7d44c38815267eac
FernandaGarcia <one.mafer@gmail.com>
2023-11-08 12:13:28 -0600

    Se sube pilepline build con DD 2

--- upload-report.py
+++ upload-report.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import argparse
+import os
 
 url_api = "http://18.218.244.166:8080/api/v2/{method}"
-api_key = "Token edaf1740e048924e2f817fb6436a803b690c6900"
+api_key = os.environ["SECRET_TOKEN"]
 
 def get_products():
     headers = {
